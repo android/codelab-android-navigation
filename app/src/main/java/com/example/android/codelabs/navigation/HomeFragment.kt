f2d01542e13d130eae9728e4a530792d7036f8d2
Jack Nelson <jnelson.java@gmail.com>
2021-03-18 22:36:30 -0700

    navigation graph and transitions

--- app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
+++ app/src/main/java/com/example/android/codelabs/navigation/HomeFragment.kt
@@ -18,7 +18,10 @@ package com.example.android.codelabs.navigation
 
 import android.os.Bundle
 import android.view.*
+import android.widget.Button
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.navOptions
 
 /**
  * Fragment used to show how to navigate to another destination
@@ -37,6 +40,11 @@ class HomeFragment : Fragment() {
         super.onViewCreated(view, savedInstanceState)
 
         //TODO STEP 5 - Set an OnClickListener, using Navigation.createNavigateOnClickListener()
+        val button = view.findViewById<Button>(R.id.navigate_destination_button)
+        button?.setOnClickListener {
+            findNavController().navigate(R.id.flow_step_one_dest, null);
+        }
+
 //        val button = view.findViewById<Button>(R.id.navigate_destination_button)
 //        button?.setOnClickListener {
 //            findNavController().navigate(R.id.flow_step_one_dest, null)
@@ -44,6 +52,18 @@ class HomeFragment : Fragment() {
         //TODO END STEP 5
 
         //TODO STEP 6 - Set NavOptions
+        val options = navOptions {
+            anim {
+                enter = R.anim.slide_in_right
+                exit = R.anim.slide_out_right
+                popEnter = R.anim.slide_in_left
+                popExit = R.anim.slide_out_left
+            }
+        }
+        view.findViewById<Button>(R.id.navigate_destination_button)?.setOnClickListener {
+            findNavController().navigate(R.id.flow_step_one_dest, null, options);
+        }
+
 //        val options = navOptions {
 //            anim {
 //                enter = R.anim.slide_in_right
